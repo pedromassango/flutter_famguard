895b7a52f04611160aea7e1ac120504e0e6fc5c8
pedromassango <pedromassango.developer@gmail.com>
2019-01-03 16:04:42 +0100

    card member done

---
+++ lib/ui/components/MemberCard.dart
@@ -0,0 +1,30 @@
+import 'package:famguard/ui/components/CircularImage.dart';
+import 'package:flutter/material.dart';
+
+class MemberCard extends StatelessWidget {
+  final String imageUrl =
+      "https://celebritypets.net/wp-content/uploads/2016/12/Adriana-Lima.jpg";
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 10,
+      width: 100,
+      child: Card(
+        elevation: 2,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: <Widget>[
+              CircularImage(NetworkImage(imageUrl)),
+              Padding(
+                padding: const EdgeInsets.only(left: 16),
+                child: Text('Massango'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
