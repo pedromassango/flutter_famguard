c2a821c036fb7b64d87f8e4ccd0750112996e6a3
pedromassango <pedromassango.developer@gmail.com>
2019-01-05 03:48:24 +0100

    added track card list in track page

---
+++ lib/ui/components/TrackCard.dart
@@ -0,0 +1,58 @@
+import 'package:famguard/data/Values.dart';
+import 'package:famguard/ui/components/CircularImage.dart';
+import 'package:flutter/material.dart';
+
+class TrackCard extends StatelessWidget{
+  final String member;
+
+  TrackCard(this.member);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: 4, right: 4),
+      child: Card(
+        elevation: 2,
+        child: Padding(
+          padding: const EdgeInsets.only(top: 12, bottom: 8, right: 8),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(left: 16, right: 16),
+                    child: CircularImage(NetworkImage('https://celebritypets.net/wp-content/uploads/2016/12/Adriana-Lima.jpg')),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(member,),
+                      Text('0.8 KM for your position',
+                        style: TextStyle(
+                            fontSize: 12,
+                            color: colorGradientTop
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+
+              Row(
+                children: <Widget>[
+                  Switch(
+                    value: true,
+                    activeColor: colorGradientTop,
+                    onChanged: (currentValue){},
+                  ),
+                  Text('Track')
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/ui/pages/tracks_page.dart
+++ lib/ui/pages/tracks_page.dart
@@ -1,10 +1,24 @@
+import 'package:famguard/data/Values.dart';
+import 'package:famguard/ui/components/TrackCard.dart';
 import 'package:flutter/material.dart';
 
-class TracksPage extends StatelessWidget{
+class TracksPage extends StatelessWidget {
+  List<String> members = ['Emily', 'Paula'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: colorGradientTop,
+        title: Text("Track"),
+      ),
+      body: ListView.builder(
+        itemCount: members.length,
+        itemBuilder: (context, index) {
+          return TrackCard(members.elementAt(index));
+        },
+      ),
     );
   }
 }
