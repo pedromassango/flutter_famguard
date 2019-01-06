42534eeb348b23a41adfdb36df9937a2339460ba
pedromassango <pedromassango.developer@gmail.com>
2019-01-06 03:31:18 +0100

    login page almost done

--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:famguard/data/values.dart';
 
-class LoginPage extends StatefulWidget{
-
+class LoginPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return _LoginPageState();
@@ -10,15 +9,73 @@ class LoginPage extends StatefulWidget{
 }
 
 class _LoginPageState extends State<LoginPage> {
+  Widget buildAppLogo() {
+    return Hero(
+      tag: "app_icon",
+      child: ImageIcon(
+        AssetImage('images/app_icon.png'),
+        size: 80,
+        color: Colors.white,
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(
-          gradient: appGradient
+        decoration: BoxDecoration(gradient: appGradient),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Spacer(),
+              Column(
+                children: <Widget>[
+                  buildAppLogo(),
+                  Text(
+                    appName,
+                    style: TextStyle(
+                      fontSize: 22,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+                ],
+              ),
+              Spacer(),
+              Container(
+                width: double.maxFinite,
+                margin: EdgeInsets.only(top: 32),
+                padding:
+                    EdgeInsets.only(left: 16, right: 16, top: 32, bottom: 12),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(16),
+                    topLeft: Radius.circular(16),
+                  ),
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Text('Use the button bellow to authenticate using your Google Account.'),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8),
+                      child: FlatButton(
+                        child: Text(
+                          "Google SignIn".toUpperCase(),
+                          style: TextStyle(color: colorGradientBottom),
+                        ),
+                        onPressed: () {
+                          Navigator.of(context).pushNamed('/home');
+                        },
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
-
       ),
     );
   }
